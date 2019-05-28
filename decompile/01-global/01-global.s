806dce35f3a5e6698f1172a67c4ae0374395dd6e
musgravejw <musgravejw@gmail.com>
2019-05-28 00:20:17 -0400

    Changes to Makefile

--- decompile/01-global/01-global.s
+++ decompile/01-global/01-global.s
@@ -1,24 +1,15 @@
 	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 14
+	.build_version macos, 10, 14	sdk_version 10, 14
 	.intel_syntax noprefix
-	.globl	_main
+	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
-	.cfi_startproc
-## BB#0:
+## %bb.0:
 	push	rbp
-Lcfi0:
-	.cfi_def_cfa_offset 16
-Lcfi1:
-	.cfi_offset rbp, -16
 	mov	rbp, rsp
-Lcfi2:
-	.cfi_def_cfa_register rbp
-	xor	eax, eax
 	pop	rbp
 	ret
-	.cfi_endproc
-
+                                        ## -- End function
 	.section	__DATA,__data
 	.globl	_a                      ## @a
 	.p2align	2
